8b5f607125f9db8fc83a7a64734f94eb7b3c11d9
WujinK <kimwj0728@naver.com>
2022-04-07 19:34:45 +0900

    4_7

--- 4_7.py
+++ 4_7.py
@@ -2,3 +2,18 @@
 # pip install numpy
 # pip install pandas
 # pip install matplotlib
+'''
+countplot => 흔히 알고 있는 막대 그래프 // 기존에 존재하는 데이터 프레임을 대상으로 시각화
+displot (함수임) => 구간별로 밀도 표현 가능 , 데이터의 전반적인 분포를 파악하는데 좋다.
+heat map => 아름다운 시각화 장점이 가장 잘 드러나는 그래프
+pairplot => 컬럼 간의 상관관계를 가늠해보는데 유용
+violinplot => 컬럼에 대한 데이터 비교분포를 표현할 때 유용
+lmplot => 산점도와 추세ㅔ선이 한 번에 합쳐진 그래프
+joinplot => 산점도와 분포를 동시에 그려주는 그래프, 그래프의 종류 지정 가능
+
+iris
+titanic
+tips => 팁
+flights => 항공데이터
+
+'''
