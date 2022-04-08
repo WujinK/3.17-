a2be7e4d5d8d12725f8dc40916cf4cbb8655f9d9
WujinK <kimwj0728@naver.com>
2022-04-08 19:36:11 +0900

    4_8.ls

--- 4_8_ls.py
+++ 4_8_ls.py
@@ -9,5 +9,15 @@ DB
 
 
 '''
-sqlite3
-'''
+<sqlite3>
+import sqlite3
+sqlite3.version
+# 버전 확인
+
+connect => 데이터 베이스 생성하는 함수
+con.cursor() // 호출하는 법 // 데이터 베이스는 엑셀과 굉장히 유사함
+
+CREATE TABLE 만들 테이블 이름(컬럼이름 데이테타입, 컬럼이름 데이터타입)
+CREATE VIEW
+CREATE INDEX
+''' 
